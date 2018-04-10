2317d7850f8918f303f74d4a0fd96d56fdafcd2a
Alan <ahgv1195@gmail.com>
2018-04-10 18:21:43 -0500

    new home design

--- assets/javascripts/application.js
+++ assets/javascripts/application.js
@@ -1,6 +1,49 @@
 $(document).ready(function() {
+	carousel();
 	catFilters();
 });
+
+function carousel() {
+	$('.owl-carousel').owlCarousel({
+		autoplay: true,
+		autoplaySpeed: 1000,
+		autoplayTimeout: 9000,
+		items: 1,
+		onChanged: callback,
+		rewind: true
+	});
+}
+
+function heroCopy() {
+	var copyHeight = $('.hero-text .active').innerHeight();
+
+	$('.hero-text').css('height', copyHeight);
+}
+
+function callback(event) {
+  var item = event.item.index;
+
+  switch (item) {
+		case 0:
+			$('.hero-copy').removeClass('active');
+			$('.copy-1').addClass('active');
+			break;
+		case 1:
+			$('.hero-copy').removeClass('active');
+			$('.copy-2').addClass('active');
+			break;
+		case 2:
+			$('.hero-copy').removeClass('active');
+			$('.copy-3').addClass('active');
+			break;
+		case 3:
+			$('.hero-copy').removeClass('active');
+			$('.copy-4').addClass('active');
+			break;
+	}
+
+	heroCopy();
+}
 
 function catFilters() {
 	$('.categories-more').on('click', function() {
